63652f645c13c23a210bf497e0ed18ab833a4cce
TJOCKochFIN <viktorhjoh@gmail.com>
2020-09-14 17:47:29 +0200

    Created some basic xml files for the reaction test game

--- app/src/main/java/com/example/graymatter/MainActivity.java
+++ app/src/main/java/com/example/graymatter/MainActivity.java
@@ -2,20 +2,14 @@ package com.example.graymatter;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.reaction_test);
 
     }
 
