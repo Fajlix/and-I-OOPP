239ad43ad93517509593fd01226005573acb5513
Felix Oliv <felano00@gmail.com>
2020-09-14 17:28:07 +0200

    Working reactionTime logic

---
+++ app/src/main/java/com/example/graymatter/ReactionTime.java
@@ -0,0 +1,62 @@
+package com.example.graymatter;
+
+import android.os.Handler;
+
+public class ReactionTime {
+    private long startTime;
+    private long endTime;
+    private long waitTime;
+    //max wait time in ms
+    private int maxWaitTime = 3000;
+    //min wait time in ms
+    private int minWaitTime = 500;
+    private Handler handler = new Handler();
+    private boolean running = false;
+    public ReactionTime(){
+        startTime = 0;
+    }
+
+    //Call this to start a new reactionTest
+    public void StartTest() {
+
+        // a random time between min- and maxWaitTime
+        waitTime = Math.round(Math.random() * maxWaitTime) + minWaitTime;
+        //TODO here we change color to "Waitcolor"
+        //just for debug atm
+        System.out.println("ready");
+        //This is used to make sure test is not stopped before waitTime is over
+        running = true;
+        // New thread that runs after waitTime
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //sets startTime to current time
+                startTime = System.currentTimeMillis();
+                if (running) {
+                    //TODO here we change color to "clickColor"
+                    //just for debug atm
+                    System.out.println("NOW");
+                }
+            }
+        },waitTime);
+
+    }
+    //Call this to stop the reactionTest returns -1 if clicked to early
+    public int StopTest(){
+            int result;
+            //makes sure that "click now" is not printed if clicked to early
+            running = false;
+            endTime = System.currentTimeMillis();
+            //If starTime is not set than button is clicked before waitTime is over
+            if (startTime == 0) {
+                result = -1;
+            } else {
+                result = (int) (endTime - startTime);
+            }
+            // resets startTime
+            startTime = 0;
+            //For debug purpose logs result
+            System.out.println(result);
+            return result;
+    }
+}
--- app/src/main/java/com/example/graymatter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/graymatter/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,17 +14,35 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.graymatter.R;
+import com.example.graymatter.ReactionTime;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private ReactionTime reactionTime;
+    private boolean firstClick = true;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        final Button reactButton = root.findViewById(R.id.react_button);
+        reactionTime = new ReactionTime();
+        reactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("pressed");
+                if (firstClick) {
+                    firstClick = false;
+                    reactionTime.StartTest();
+                }
+                else{
+                    reactionTime.StopTest();
+                    firstClick = true;
+                }
+            }
+        });
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
