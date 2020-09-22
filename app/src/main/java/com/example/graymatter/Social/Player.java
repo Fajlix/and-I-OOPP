fa69e517c94fb44b3dfeb346d310eb2a61cc70a5
Aline <aline.eikeland@gmail.com>
2020-09-22 23:56:38 +0200

    Fixes Player + addition .gitignore
    Player does now compile. .gitignore now ignores workspace.xml

--- app/src/main/java/com/example/graymatter/Social/Player.java
+++ app/src/main/java/com/example/graymatter/Social/Player.java
@@ -1,11 +1,17 @@
 package com.example.graymatter.Social;
 
-import java.awt.Image;
+
+import android.media.Image;
+import android.provider.ContactsContract;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class Player {
 
-
+    //tveksam lösning
+    private static String defaultPath = "";
     //  private UserInfo userInfo;
     /**
      * Needed upon registration:
@@ -20,6 +26,9 @@ public class Player {
      */
     private int userKey;
 
+
+    //tänk på bilderna!!!
+    private String imagePath;
     private Image userImage;
     private List<GameSession> playerHistory;
     private List<Integer> friendIDs;
@@ -29,38 +38,56 @@ public class Player {
 
     }
 
-    private Player(int userKey, int friendID, String email, String password, String userName){
-
+    private Player(int userKey, int friendID, String email, String password, String userName, String imagePath, Image userImage, List<GameSession> playerHistory, List<Integer> friendIDs) throws MissingAccessException {
+        this(friendID, userName, imagePath, userImage, playerHistory, friendIDs);
         this.userKey = userKey;
+        this.email = email;
+        this.password = password;
     }
 
-    private Player(int friendID, String userName, String imagePath, List<GameSession> playerHistory){
+    private Player(int friendID, String userName, String imagePath, Image userImage, List<GameSession> playerHistory, List<Integer> friendIDs) throws MissingAccessException {
         //assign public fields, mark others "0"
+        this.friendID = friendID;
+        this.userName = userName;
+        /*
+        this.userImage = null;
+        try {
+            userImage = ImageIO.read(new File("strawberry.jpg"));
+        } catch (IOException e) {
+            userImage = ImageIO.read(new File(defaultPath));
+        }
+         **/
+        this.playerHistory = playerHistory;
+        this.friendIDs = friendIDs;
     }
 
-    public static Player makePlayer(int userKey, int friendID, String email, String password, String userName){
-
-        this.userKey = userKey;
+    public Player (Player player) throws MissingAccessException {
+        this(player.userKey, player.friendID, player.email, player.password, player.userName, player.imagePath, player.userImage, player.playerHistory, player.friendIDs);
     }
 
-    public static Player makePublicPlayer(int friendID, String userName, String imagePath, List<GameSession> playerHistory){
-        return new Player(friendID, userName, imagePath, playerHistory);
+    public static Player makePlayer(int userKey, int friendID, String email, String password, String userName, String imagePath, Image userImage, List<GameSession> playerHistory, List<Integer> friendIDs) throws MissingAccessException {
+        return new Player(userKey, friendID, email, password, userName, imagePath, userImage, playerHistory, friendIDs);
+
+    }
+
+    public static Player makePublicPlayer(int friendID, String userName, String imagePath, Image userImage, List<GameSession> playerHistory, List<Integer> friendIDs) throws MissingAccessException {
+        return new Player(friendID, userName, imagePath, userImage, playerHistory, friendIDs);
     }
 
 
-    public static Player getPlayer(int friendID, int userKey) implements Throwable{
-        if(this.userKey = userKey){
+    public Player getPlayer(int friendID, int userKey) throws MissingAccessException {
+        if(this.userKey == userKey){
             throw new MissingAccessException("Userkey does not match");
         }
-        return Player(userKey, friendID);
+        return new Player(this);
     }
 
     public List<Integer> getFriendIDs(){
-
+        return friendIDs;
     }
 
     public void setPassword(String oldPassword, String newPassword, int userKey) {
-        if(this.userKey==userKey && this.password==oldPassword) this.password = newPassword;
+        if(this.userKey==userKey && this.password.equals(oldPassword)) this.password = newPassword;
     }
 
     public void setEmail(String email, int userKey) {
@@ -81,6 +108,7 @@ public class Player {
         return userImage;
     }
 
+    //needs equals
 
     public void changeUserKey(int oldUserKey, int newUserKey){
 
@@ -88,9 +116,13 @@ public class Player {
 
 
 
-    private class MissingAccessException extends Exception {
+    private static class MissingAccessException extends Exception {
         public MissingAccessException(String exceptionLabel){
             super(exceptionLabel);
+        }
+
+        public MissingAccessException(){
+            super();
         }
     }
 
