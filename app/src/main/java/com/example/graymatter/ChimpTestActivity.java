cf3eef46d13a58cf625c2c4f8ae06577e5c2f272
TJOCKochFIN <viktorhjoh@gmail.com>
2020-09-23 15:20:18 +0200

    a start to create xml components

---
+++ app/src/main/java/com/example/graymatter/ChimpTestActivity.java
@@ -0,0 +1,4 @@
+package com.example.graymatter;
+
+public class ChimpTestActivity {
+}
---
+++ app/src/main/java/com/example/graymatter/ChimpTestButton.java
@@ -0,0 +1,16 @@
+package com.example.graymatter;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
+import java.util.zip.Inflater;
+
+public class ChimpTestButton extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+    }
+}
