a33d5732aa11a5cadcb2166a4bae43d1fa82f7a1
Felix Oliv <felano00@gmail.com>
2020-09-30 19:28:25 +0200

    Fixed test in ReactionTimeGameTest

--- app/src/test/java/com/example/graymatter/Model/Game/ReactionTimeGameTest.java
+++ app/src/test/java/com/example/graymatter/Model/Game/ReactionTimeGameTest.java
@@ -1,26 +1,35 @@
 package com.example.graymatter.Model.Game;
 
 import com.example.graymatter.Model.Game.ReactionGame.ReactionTimeGame;
+import com.example.graymatter.ViewModel.ReactionTimeViewModel;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.TimerTask;
+
 import static org.junit.Assert.*;
 
 public class ReactionTimeGameTest {
+    private TimerTask task;
+    private int sleepMillis;
     @Before
     public void setUp(){
-
+        sleepMillis = 1000;
     }
 
     @Test
-    public void startAndStopTest() throws InterruptedException {
+    public void reactionTimeCorrect() throws InterruptedException {
         ReactionTimeGame reactionTime = new ReactionTimeGame();
         reactionTime.startGame();
-        int res = reactionTime.endGame();
-        assertEquals(res, -1);
+        reactionTime.setStartTime(System.currentTimeMillis());
+        Thread.sleep(sleepMillis);
+        assertTrue(Math.abs(sleepMillis - reactionTime.endGame()) < sleepMillis/10);
+    }
+    @Test
+    public void reactionTimeTooFast() throws InterruptedException {
+        ReactionTimeGame reactionTime = new ReactionTimeGame();
         reactionTime.startGame();
-        Thread.sleep(reactionTime.getRandWaitTime() + 1000);
-        assertTrue(reactionTime.endGame()>0);
+        assertEquals(reactionTime.endGame(), -1);
     }
 }
