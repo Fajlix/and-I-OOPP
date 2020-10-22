6eae1323c9e64f512171643923432b17736b24aa
Felix Oliv <felano00@gmail.com>
2020-10-22 16:55:34 +0200

    added a import

--- app/src/main/java/com/example/graymatter/Model/dataAccess/dataMapperImplementation/GameSessionMapper.java
+++ app/src/main/java/com/example/graymatter/Model/dataAccess/dataMapperImplementation/GameSessionMapper.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.util.List;
 import java.util.Optional;
 
